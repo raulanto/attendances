fc8b42c116637fe116bb4d7f3f25596e1dca232a
raul <74162376+raulanto@users.noreply.github.com>
2023-12-09 14:16:20 -0600

    action guardar

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=attendancebd',
+    'dsn' => 'mysql:host=localhost;dbname=multicheckl',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
